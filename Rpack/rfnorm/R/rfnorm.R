a58af9b6e27bfe9211d1d56989fb54e0f5e8c565
Yifan <yfyang86@users.noreply.github.com>
2023-01-12 20:51:04 +0800

    [DEV] R package

--- Rpack/rfnorm/R/rfnorm.R
+++ Rpack/rfnorm/R/rfnorm.R
@@ -4,11 +4,13 @@
 #' @param n sample size
 #' @param mu mean vector
 #' @param Sigma Variance matrix, should be postive, set checkSigma = TRUE to check the postiveness
+#' @param method "Gibbs", "HMC", "mvtnorm", default = "Gibbs"
 #' @param init initial value (matrix) for Gibbs sampler with p x chain_n dimesions. p is the variable dimension, chain_n is the number of chains.
 #' @param gibbs_length iteration length for Gibbs sampler, default = 1000
 #' @param gibbs_chain number of chains for Gibbs sampler, default = 4
 #' @param checkSigma TRUE/FALSE If TRUE, then mvrfnorm checks whether the variance matrix is positive or not.
 #' @return sample array list
+#'          - method "Gibbs" or "HMC" or "mvtnorm"
 #' @examples
 #' ux = matrix(rnorm(4), 2, 2)
 #' Sig = ux %*% t(ux)
@@ -26,35 +28,51 @@ mvrfnorm <- function(n, mu, Sigma, method="Gibbs",
                     init = matrix(rep(0.5, length(mu)*4), nrow = 4), 
                     gibbs_length=1000, gibbs_chain = 4, 
                     checkSigma = FALSE){
-if (sum(method %in% "Gibbs") != 1){
+if (sum(method %in% c("Gibbs", "HMC", "mvtnorm")) != 1){
     method <- "Gibbs"
 }
 
 p = length(mu)
 init = matrix(init, ncol = p)
 
+
+## p check
+if(p < 2){
+    stop("Dimension should be larger than one!!!");
+}
+
 ### n chain
 if(dim(init)[1] != gibbs_chain){
-    stop('Number of chain not match.');
+    stop('Number of chain does not match!!!');
 }
 
 if (length(mu) != dim(init)[2]){
-    stop("Length of initial value must be the same as the dimension.")
+    stop("Length of initial value must be the same as the dimension!!!")
 }
-
-
-if(p != 2){
-    stop("Release in the furture");
-}
-
 
 ## assert Variance-Cov Matrix
 if(checkSigma){
     if( sum(svd(Sigma)$d > 0 ) < p){
-    stop("Variance Covariance Matrix is not positive definite.")
+    stop("Variance Covariance Matrix is not positive definite!!!")
 }}
 
-#### DIM == 2 ####
+if(method == "mvtnorm") return(
+    list(
+        method = "mvtnorm",
+        value = abs(
+            rmvnorm(n, mu, Sigma)
+        )
+    )
+)
+
+if(method == "HMC") {
+    stop("Release HMC in the furture.");
+} 
+
+#### Gibbs DIM == 2####
+if(p != 2){
+    stop("Release dim > 2 with Gibbs sampler in the furture.");
+}
 sig1 <- sqrt(Sigma[1, 1])
 sig2 <- sqrt(Sigma[2, 2])
 rho <- Sigma[1, 2]/sig1/sig2
@@ -78,5 +96,5 @@ dim3 <- paste0("theta", 1:p)
 sample_array <- array(c(sample_p1,sample_p2),c(n, gibbs_chain, p),dimnames = list(NULL,dim2,dim3))
 names(dimnames(sample_array)) <- c("Iteration", "Chain", "Parameter")
 
-return(sample_array);
+return(list(method = method, value = sample_array));
 }
